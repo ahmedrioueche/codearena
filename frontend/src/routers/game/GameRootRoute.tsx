67478a5cda843aed3f8647cc200512ab9c0eb68b
Ahmed Drioueche <adsrahmed@gmail.com>
2025-05-03 00:00:25 +0100

    fix

--- frontend/src/routers/game/GameRootRoute.tsx
+++ frontend/src/routers/game/GameRootRoute.tsx
@@ -1,9 +1,14 @@
 import { createRoute } from "@tanstack/react-router";
 import { RootRoute } from "../rootRoute";
 import GamePage from "../../app/pages/game/pages/GamePage";
+import { ProtectedRoute } from "../../components/ProtectedRoute";
 
 export const GameRootRoute = createRoute({
   getParentRoute: () => RootRoute,
   path: "/game",
-  component: () => <GamePage />,
+  component: () => (
+    <ProtectedRoute>
+      <GamePage />
+    </ProtectedRoute>
+  ),
 });
