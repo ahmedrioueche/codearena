21682c314fd6ec93d72475e89f942db4b47bd6a9
Ahmed Drioueche <adsrahmed@gmail.com>
2025-12-26 21:25:41 +0100

    fix: gemini

--- frontend/src/utils/gemini.ts
+++ frontend/src/utils/gemini.ts
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const API_KEY: string = import.meta.env.VITE_GEMINI_KEY;
 const genAI = API_KEY ? new GoogleGenerativeAI(API_KEY) : null;
-const model = genAI?.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI?.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 const APP_DESC = `This prompt is coming form an app called CodeArena, its purpose is to helps devs get better at programming,
 you serve the inteligent backbone that runs this app through prompts.`;
